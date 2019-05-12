26c9c92cdea1c9a5fb9500533c7272fc8b991440
shoheih <shoheigithub@gmail.com>
2019-05-12 17:41:13 +0900

    apply uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const outputPath = path.resolve(__dirname, 'dist')
-console.log({outputPath})
 
 module.exports = {
     entry: './src/index.js',
@@ -51,5 +51,14 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].[hash].css'
         })
-    ]
+    ],
+    optimization: {
+        minimizer: [new UglifyJsPlugin({
+            uglifyOptions: {
+                compress: {
+                    drop_console: true
+                }
+            }
+        })]
+    }
 }
