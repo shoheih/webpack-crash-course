22c1702af7b0011269b5a7152bf3eea0baea5ca8
shoheih <shoheigithub@gmail.com>
2019-05-12 09:57:41 +0900

    implement url-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,15 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader'
-                ]
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|ico)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 2048,
+                    name: './images/[name].[ext]'
+                },
             }
         ]
     },
